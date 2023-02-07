59fde21a72a18df0d8c4d3b435b0e227340d6753
Gobinath7878 <gobinaths7878@gmail.com>
2023-02-07 11:28:14 +0530

    url commit

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,1 @@
-export const BASE_URL = 'http://localhost:4000/api/v1';
-// export const REG_URL ='http://localhost:4000/api/v1/auth/register'
+export const BASE_URL = 'https://tour-app-be.onrender.com';
