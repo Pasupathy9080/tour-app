104007c0223a52fcea755fc39f6e2550a4038110
Gobinath S <115769414+Gobinath7878@users.noreply.github.com>
2023-02-07 11:53:23 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
 
       try{
           
-        const res = await fetch(`${BASE_URL}/auth/login`,{
+        const res = await fetch(`${BASE_URL}/api/v1/auth/login`,{
           method:"post",
          headers:{
         "content-type":"application/json",
