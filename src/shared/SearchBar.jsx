e71c8979b2817dfb784fbbd2d69a6a4eab6fc0f9
Gobinath7878 <gobinaths7878@gmail.com>
2023-02-07 12:21:01 +0530

    seven

--- src/shared/SearchBar.jsx
+++ src/shared/SearchBar.jsx
@@ -31,7 +31,7 @@ const SearchBar = () => {
 
 
     const result =await res.json()
-    navigate(`/api/v1/tours/search?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`,{state: result.data})
+    navigate(`/tours/search?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}`,{state: result.data})
 
 
     // if(location==='' || distance==='' || maxGroupSize===''){
